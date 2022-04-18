21df1d8b03ca937570f6a9d086ab161f6f0eb2e6
q.lescut <q.lescut@gmail.com>
2022-04-19 00:59:21 +0200

    php

--- src/app/ApiRest.php
+++ src/app/ApiRest.php
@@ -2,34 +2,87 @@
        //Connexion à la base de données
        try
        {
-              $bdd = new PDO('mysql:host=;dbname:testtechniqueivs', 'root', '');
-              echo("connecté");
+              $pdo = new PDO('mysql:host=localhost;dbname=ivs', 'root', '');
        }
 
-       catch(Exception $error)
+       catch(PDOException $error)
        {
               die('Erreur : '.$error->getMessage());
        }
 
-       //récupération des éléments de la table pieces
-       $pieces = 'SELECT name, personnes_presentes FROM pieces';
-       //affichage des éléments récupérés de la table pieces
-       foreach ($bdd->query($pieces) as $row) {
-              print $row['name'] . "\t";
-              print $row['personnes_presentes'] . "\n";
-       }      
 
-       //récupération des éléments de la table buildings
-       $buildings = $bdd->prepare('SELECT name, zipcode, personnes_presentes FROM buildings');
-       //affichage des éléments récupérés de la table buildings
-       $buildings_result = $buildings->fetch(PDO::FETCH_BOTH);
-       print_r($buildings_result ); 
-       print("\n");
 
-       //récupération des éléments de la table organisations
-       $organisations = $bdd->prepare('SELECT name, personnes_presentes FROM organisations');
-       //affichage des éléments récupérés de la table organisations
-       $organisations_result = $organisations->fetch(PDO::FETCH_BOTH);
-       print_r($organisations_result ); 
-       print("\n");
+       //récupération des éléments de la table pieces en préparant une requête SQl
+       $sqlpieces = 'SELECT id, name, presents FROM pieces';
+       //préparation de la requete 
+       $statementpieces = $pdo->prepare($sqlpieces);
+       // execution de la requete
+       $statementpieces->execute(array());
+       // afficher les éléments de la table
+       while (($rowpieces = $statementpieces->fetch(PDO::FETCH_ASSOC))) {
+           print $rowpieces['id'];
+           print $rowpieces['name'];
+           print $rowpieces['presents'];
+       }
+
+
+
+       //récupération des éléments de la table buildings en préparant une requête SQl
+       $piecespresents = 'SELECT presents FROM pieces';
+       //création du tableau
+       $arraypieces = array();
+       //préparation de la requete 
+       $statpiecespresents = $pdo->prepare($piecespresents);
+       // execution de la requete
+       $statpiecespresents->execute($arraypieces);   
+
+       //récupération des élements venant de la bdd
+       $piecesbuildings = 'SELECT buildings FROM pieces';
+       $buildingsname = 'SELECT name FROM buildings';
+
+       //récupération des éléments de la table buildings en préparant une requête SQl
+       $sqlbuildings = 'SELECT id, name, zipcode FROM buildings';
+       //préparation de la requete 
+       $statementbuildings = $pdo->prepare($sqlbuildings);
+       // execution de la requete
+       $statementbuildings->execute(array());
+       // afficher les éléments de la table
+       while (($rowbuildings = $statementbuildings->fetch(PDO::FETCH_ASSOC))) {
+           print $rowbuildings['id'];
+           print $rowbuildings['name'];
+           print $rowbuildings['zipcode'];
+           if($piecesbuildings = $buildingsname) {
+              print array_sum($arraypieces);
+           }
+       }
+
+
+
+       //récupération des éléments de la table buildings en préparant une requête SQl
+       $buildingspresents = 'SELECT presents FROM buildings';
+       //création du tableau
+       $arraybuildings = array();
+       //préparation de la requete 
+       $statbuildingspresents = $pdo->prepare($buildingspresents);
+       // execution de la requete
+       $statbuildingspresents->execute($arraybuildings);   
+
+       //récupération des élements venant de la bdd
+       $buildingsorganisations = 'SELECT organisations FROM buildings';
+       $organisationsname = 'SELECT name FROM organisations';
+
+       //récupération des éléments de la table organisations en préparant une requête SQl
+       $sqlorganisations = 'SELECT id, name FROM organisations';
+       //préparation de la requete 
+       $statementorganisations = $pdo->prepare($sqlorganisations);
+       // execution de la requete
+       $statementorganisations->execute(array());
+       // afficher les éléments de la table
+       while (($roworganisations = $statementorganisations->fetch(PDO::FETCH_ASSOC))) {
+           print $roworganisations['id'];
+           print $roworganisations['name'];
+           if ($buildingsorganisations = $organisationsname) {
+              print array_sum($arraybuildings);
+           }
+       }
 ?>
